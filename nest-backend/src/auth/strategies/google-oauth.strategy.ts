b92a7e1593a580feade3f74ca408fa85cf075429
nevermind1727 <dmit.taran2017@gmail.com>
2023-03-07 20:18:15 +0200

    done function added

--- nest-backend/src/auth/strategies/google-oauth.strategy.ts
+++ nest-backend/src/auth/strategies/google-oauth.strategy.ts
@@ -1,5 +1,5 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Profile, Strategy } from 'passport-google-oauth20';
+import { Profile, Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Types } from 'mongoose';
@@ -20,13 +20,15 @@ export class GoogleOauthStrategy extends PassportStrategy(Strategy, 'google') {
     _accessToken: string,
     _refreshToken: string,
     profile: Profile,
+    done: VerifyCallback,
   ) {
     const { emails } = profile;
     const _id = new Types.ObjectId();
-    console.log('Inside google strategy');
-    return {
+    const user = {
       username: emails[0].value,
       _id,
     };
+    console.log('Inside google strategy');
+    done(null, user);
   }
 }
