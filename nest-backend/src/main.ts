d3aed4c91582e61102661944dd8a183bae4f8673
nevermind1727 <dmit.taran2017@gmail.com>
2023-03-08 15:26:04 +0200

    asjdlksa

--- nest-backend/src/main.ts
+++ nest-backend/src/main.ts
@@ -7,7 +7,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://student-helper-two.vercel.app/',
+    origin: 'https://student-helper-two.vercel.app',
     credentials: true,
   });
   app.use(cookieParser());
